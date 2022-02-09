e49b222c777fed6a587fedb2fa3e6d9be55c2983
Cesar Torres <torres-cesar@outlook.com>
2022-02-09 16:15:42 -0600

    Add operator functions

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function add(valueOne, valueTwo) {
+    let sum = valueOne + valueTwo;
+    return sum;
+}
+
+function subtract(valueOne, valueTwo) {
+    let difference = valueOne - valueTwo;
+    return difference;
+}
+
+function multiply(valueOne, valueTwo) {
+    let product = valueOne * valueTwo;
+    return product;
+}
+
+function divide(valueOne, valueTwo) {
+    let quotient = valueOne / valueTwo;
+    return quotient;
+}
+
+let inputOne = 3;
+let inputTwo = 5;
+
+console.log(divide(inputOne, inputTwo));
